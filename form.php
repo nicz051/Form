f405288f0c431c8449bb58601c9c31a1888558d9
nicz051 <bnicolepenny@gmail.com>
2021-06-22 01:48:46 +0800

    create form and display
    display name and age

---
+++ form.php
@@ -0,0 +1,30 @@
+
+
+<html>
+
+    <body>
+    <form action="display.php" method="get">
+    Name: <input type="text" name="name"><br>
+    Age: <input type="text" name="age"><br>
+    <input type="submit">
+
+    </body>
+
+
+
+</html>
+
+
+
+
+
+<?php
+
+
+
+
+
+
+
+
+?>
